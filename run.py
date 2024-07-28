2e3564e28822090efd7d00a5f50d8aba4625a9f9
cancanbak <cancanbaki99@gmail.com>
2024-07-28 15:00:55 +0300

    a

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import twint
 
-# Kullanıcı adlarını liste olarak tanımla
 usernames = [
     "WatcherGuru", "VitalikButerin", "rovercrc", "_RichardTeng", "justinsuntron", "BitcoinMagazine",
     "Cointelegraph", "CryptoCapo_", "TheCryptoLark", "binance", "zerohedge", "saylor", "lookonchain",
@@ -8,14 +7,18 @@ usernames = [
     "MMCrypto", "BTC_Archive", "cz_binance"
 ]
 
-# Her bir kullanıcı için Twint konfigürasyonunu çalıştır
 for username in usernames:
-    c = twint.Config()
-    c.Username = username
-    c.Limit = 100  # İstediğin tweet sayısını buraya yaz
-    c.Store_csv = True
-    c.Output = "tweets.csv"  # Çıktı dosyasının adı, aynı dosyaya eklemek için aynı adı kullan
-    c.Custom["tweet"] = ["date", "tweet", "username", "id", "link", "retweets", "likes", "replies", "hashtags"]
-    c.Resume = "tweets.csv"  # Kaldığı yerden devam etmesi için aynı dosya adı
-
-    twint.run.Search(c)
+    try:
+        c = twint.Config()
+        c.Username = username
+        c.Limit = 100
+        c.Store_csv = True
+        c.Output = "tweets.csv"
+        c.Custom["tweet"] = ["date", "tweet", "username", "id", "link", "retweets", "likes", "replies", "hashtags"]
+        c.Resume = "tweets.csv"
+        
+        print(f"Fetching tweets for {username}")
+        twint.run.Search(c)
+        
+    except Exception as e:
+        print(f"Error fetching tweets for {username}: {e}")
